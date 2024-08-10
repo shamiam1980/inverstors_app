35cb0a243c1b7ea365c4febbeb01b01f16a0b353
Hesham El Querm <el.erm.el.erm@gmail.com>
2024-08-10 11:09:47 +0300

    Added navbar to homepage Minor UI updates

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,23 +1,28 @@
 import React from "react";
+import Container from "react-bootstrap/Container";
 import { NavLink } from "react-router-dom";
+import Navbar from "../Navbar/Navbar";
 import "./Home.css";
 
 const Home = () => {
   return (
     <div className='home wrapper'>
       <div className='qobba wrapper'> </div>
-      <div className='home-content'>
-        الصفحة الرئيسية
-        <div>
-          <NavLink
+      <Navbar />
+      <Container fluid='xxl'>
+        <div className='home-content'>
+          <div className='home-title'>رأس المال ونوع الاستثمار</div>
+        </div>
+      </Container>
+      <div>
+        {/* <NavLink
             to='/'
             exact
             className='login-navlink'
             title='To Login'
             activeStyle={{ color: "#f7f7f7", fontWeight: "bold" }}>
             تسجيل خروج
-          </NavLink>
-        </div>
+          </NavLink> */}
       </div>
     </div>
   );
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,108 @@
+import React, { useState } from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { NavLink, Link } from "react-router-dom";
+import { useMediaQuery } from "react-responsive";
+import "./Navbar.css";
+// import logo from "../../images/logo.png";
+
+const AppNavbar = () => {
+  const [activePrompt, setActivePrompt] = useState(false);
+  const [activeBlog, setActiveBlog] = useState(false);
+  const [expanded, setExpanded] = useState(false);
+
+  const isMobileNav = useMediaQuery({ maxWidth: 992 });
+  const isSmlMobile = useMediaQuery({ maxWidth: 470 });
+
+  return (
+    <Navbar
+      collapseOnSelect
+      expand='lg'
+      expanded={isMobileNav ? expanded : null}
+      id='navbar'>
+      <Container fluid='xxl'>
+        <Navbar.Brand>
+          <div className='nav-logo flex-it'>
+            شعار
+            <br />
+            الشركة
+          </div>
+          <div className='nav-username'>
+            <span className='nav-username-text'>أهلاً</span>{" "}
+            <span className='nav-username-name'>هشام إبراهيم القرم</span>
+            <span className='nav-username-text'>,</span>
+          </div>
+          {/* <NavLink
+            to='/submit'
+            exact
+            className='nav-link nav-link-custom'
+            onClick={() => (
+              setActivePrompt(false), setActiveBlog(false), setExpanded(false)
+            )}
+            activeStyle={{ color: "var(--primary-color)", fontWeight: "bold" }}>
+            <Button className='nav-submit-btn p-ar' variant='warning'>
+              أضف مقترح نصي
+            </Button>
+          </NavLink> */}
+        </Navbar.Brand>
+        <Navbar.Toggle onClick={() => setExpanded(!expanded)} />
+        <Navbar.Collapse>
+          <Nav id='nav' className='me-auto flex-grow-1 justify-content-end'>
+            {isSmlMobile && (
+              <Nav.Item>
+                <Nav.Link disabled>
+                  <div className='nav-username-mobile'>
+                    <span className='nav-username-text'>أهلاً</span>{" "}
+                    <span className='nav-username-name'>
+                      هشام إبراهيم القرم
+                    </span>
+                    <span className='nav-username-text'>,</span>
+                  </div>
+                </Nav.Link>
+              </Nav.Item>
+            )}
+            <Nav.Item>
+              <Nav.Link disabled>
+                <svg
+                  width='18'
+                  height='18'
+                  viewBox='0 0 18 18'
+                  fill='none'
+                  xmlns='http://www.w3.org/2000/svg'>
+                  <path
+                    d='M15.75 15V3L1.5 9L15.75 15ZM14.25 12.75L5.3625 9L14.25 5.25V7.875L9.75 9L14.25 10.125V12.75ZM14.25 12.75V9V5.25V7.875V10.125V12.75Z'
+                    fill='#083937'
+                  />
+                </svg>
+                <span className='nav-item-text'>مساعدة؟</span>
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link
+                to='/'
+                exact
+                as={Link}
+                className='nav-link nav-link-custom'>
+                <svg
+                  width='18'
+                  height='18'
+                  viewBox='0 0 18 18'
+                  fill='none'
+                  xmlns='http://www.w3.org/2000/svg'>
+                  <path
+                    d='M9.75 12V5.8875L11.7 7.8375L12.75 6.75L9 3L5.25 6.75L6.3 7.8375L8.25 5.8875V12H9.75ZM13.5 15C13.9125 15 14.2656 14.8531 14.5594 14.5594C14.8531 14.2656 15 13.9125 15 13.5V11.25H13.5V13.5H4.5V11.25H3V13.5C3 13.9125 3.14687 14.2656 3.44062 14.5594C3.73438 14.8531 4.0875 15 4.5 15H13.5Z'
+                    fill='#083937'
+                  />
+                </svg>
+                <span className='nav-item-text'>خروج</span>
+              </Nav.Link>
+            </Nav.Item>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default AppNavbar;
