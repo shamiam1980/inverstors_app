582725f34c9d80937c1303a458a85a71d53b292a
Hesham El Querm <el.erm.el.erm@gmail.com>
2024-08-11 17:41:44 +0300

    Completed navbar and added mobile nav

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -12,18 +12,12 @@ const Home = () => {
       <Container fluid='xxl'>
         <div className='home-content'>
           <div className='home-title'>رأس المال ونوع الاستثمار</div>
+          <div className='home-section home-section-1'>البيانات</div>
+          <div className='home-title'>حساب الأرباح</div>
+          <div className='home-section home-section-2'>البيانات</div>
         </div>
       </Container>
-      <div>
-        {/* <NavLink
-            to='/'
-            exact
-            className='login-navlink'
-            title='To Login'
-            activeStyle={{ color: "#f7f7f7", fontWeight: "bold" }}>
-            تسجيل خروج
-          </NavLink> */}
-      </div>
+      <div></div>
     </div>
   );
 };
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
@@ -11,9 +11,36 @@ const AppNavbar = () => {
   const [activePrompt, setActivePrompt] = useState(false);
   const [activeBlog, setActiveBlog] = useState(false);
   const [expanded, setExpanded] = useState(false);
+  const [navbarOpen, setNavbarOpen] = useState(false);
+
+  const sideNavRef = useRef(null);
 
   const isMobileNav = useMediaQuery({ maxWidth: 992 });
   const isSmlMobile = useMediaQuery({ maxWidth: 470 });
+
+  useEffect(() => {
+    // Add event listener to the document object
+    document.addEventListener("mousedown", handleClickOutside);
+
+    // Remove event listener when the component unmounts
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  function handleClickOutside(event) {
+    if (sideNavRef.current && !sideNavRef.current.contains(event.target)) {
+      closeNav();
+    }
+  }
+
+  const openNav = () => {
+    setNavbarOpen(true);
+  };
+
+  const closeNav = () => {
+    setNavbarOpen(false);
+  };
 
   return (
     <Navbar
@@ -46,22 +73,9 @@ const AppNavbar = () => {
             </Button>
           </NavLink> */}
         </Navbar.Brand>
-        <Navbar.Toggle onClick={() => setExpanded(!expanded)} />
+        <Navbar.Toggle onClick={openNav} />
         <Navbar.Collapse>
           <Nav id='nav' className='me-auto flex-grow-1 justify-content-end'>
-            {isSmlMobile && (
-              <Nav.Item>
-                <Nav.Link disabled>
-                  <div className='nav-username-mobile'>
-                    <span className='nav-username-text'>أهلاً</span>{" "}
-                    <span className='nav-username-name'>
-                      هشام إبراهيم القرم
-                    </span>
-                    <span className='nav-username-text'>,</span>
-                  </div>
-                </Nav.Link>
-              </Nav.Item>
-            )}
             <Nav.Item>
               <Nav.Link disabled>
                 <svg
@@ -101,6 +115,72 @@ const AppNavbar = () => {
           </Nav>
         </Navbar.Collapse>
       </Container>
+      <nav className='navbar-mobile' aria-label='main navigation'>
+        {/* drawer */}
+        <div className='sidenav-wrapper level-left has-text-centered'>
+          <div
+            id='mySidenav'
+            ref={sideNavRef}
+            className={"sidenav " + (navbarOpen ? "opened" : "")}>
+            {/* <a className='closebtn' onClick={() => closeNav()}>
+              &times;
+            </a> */}
+            <Nav
+              id='nav-mobile'
+              className='me-auto flex-grow-1 justify-content-end'>
+              {isMobileNav && (
+                <Nav.Item className='noclick'>
+                  <Nav.Link disabled>
+                    <div className='nav-username-mobile'>
+                      <span className='nav-username-text'>أهلاً</span>{" "}
+                      <span className='nav-username-name'>
+                        هشام إبراهيم القرم
+                      </span>
+                      <span className='nav-username-text'>,</span>
+                    </div>
+                  </Nav.Link>
+                </Nav.Item>
+              )}
+              <Nav.Item>
+                <Nav.Link disabled>
+                  <svg
+                    width='18'
+                    height='18'
+                    viewBox='0 0 18 18'
+                    fill='none'
+                    xmlns='http://www.w3.org/2000/svg'>
+                    <path
+                      d='M15.75 15V3L1.5 9L15.75 15ZM14.25 12.75L5.3625 9L14.25 5.25V7.875L9.75 9L14.25 10.125V12.75ZM14.25 12.75V9V5.25V7.875V10.125V12.75Z'
+                      fill='#083937'
+                    />
+                  </svg>
+                  <span className='nav-item-text'>مساعدة؟</span>
+                </Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link
+                  to='/'
+                  exact
+                  as={Link}
+                  className='nav-link nav-link-custom'>
+                  <svg
+                    width='18'
+                    height='18'
+                    viewBox='0 0 18 18'
+                    fill='none'
+                    xmlns='http://www.w3.org/2000/svg'>
+                    <path
+                      d='M9.75 12V5.8875L11.7 7.8375L12.75 6.75L9 3L5.25 6.75L6.3 7.8375L8.25 5.8875V12H9.75ZM13.5 15C13.9125 15 14.2656 14.8531 14.5594 14.5594C14.8531 14.2656 15 13.9125 15 13.5V11.25H13.5V13.5H4.5V11.25H3V13.5C3 13.9125 3.14687 14.2656 3.44062 14.5594C3.73438 14.8531 4.0875 15 4.5 15H13.5Z'
+                      fill='#083937'
+                    />
+                  </svg>
+                  <span className='nav-item-text'>خروج</span>
+                </Nav.Link>
+              </Nav.Item>
+            </Nav>
+          </div>
+        </div>
+      </nav>
     </Navbar>
   );
 };
