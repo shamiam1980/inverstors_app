8a0540aee8a62153ad97d3489a171916f862a6a6
Hesham El Querm <el.erm.el.erm@gmail.com>
2024-08-30 11:58:21 +0300

    Preloaded main space BG image before initalizing app

--- src/App.js
+++ src/App.js
@@ -4,25 +4,40 @@ import Loader from "./components/Loader/Loader";
 import Login from "./components/Login/Login";
 import Home from "./components/Home/Home";
 import "./App.css";
+import BGImage from "../src/images/bg-space-desktop.png";
 
 const App = () => {
   const [screenLoading, setScreenLoading] = useState(false);
   const [loaded, setLoaded] = useState(false);
+  const [imageIsLoaded, setImageIsLoaded] = useState(false);
 
   useEffect(() => {
-    if (window.location.pathname.replace(/\/+$/, "").length === 0) {
-      setScreenLoading(true);
-      setTimeout(() => {
-        setScreenLoading(false);
+    let img = new Image();
+    img.src = BGImage;
+  }, [imageIsLoaded]);
+
+  useEffect(() => {
+    if (imageIsLoaded) {
+      if (window.location.pathname.replace(/\/+$/, "").length === 0) {
+        setScreenLoading(true);
+        setTimeout(() => {
+          setScreenLoading(false);
+          setLoaded(true);
+        }, 6500);
+      } else {
         setLoaded(true);
-      }, 6500);
-    } else {
-      setLoaded(true);
+      }
     }
-  }, []);
+  }, [imageIsLoaded]);
 
   return (
     <div className='App'>
+      <img
+        id='app-bg-img'
+        src={BGImage}
+        onLoad={() => setImageIsLoaded(true)}
+        style={{ display: imageIsLoaded ? "inline-block" : "none" }}
+      />
       {screenLoading ? (
         <Loader />
       ) : (
